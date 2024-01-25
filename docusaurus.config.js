c7272eaef679c25e01e9408732219f0b61e3592e
timbastin <bastin.tim@gmail.com>
2024-01-25 14:27:20 +0100

    adds core-objectives doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
     favicon: '/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://your-docusaurus-site.example.com',
+    url: process.env.DEPLOYMENT_URL || 'https://flawfix.dev',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
